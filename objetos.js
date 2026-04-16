d8dc212da6223c64d236db3ecef92ec9ddc57eef
jesur <juansurianodev@gmail.com>
2026-04-15 20:07:29 -0600

    Agregando practicas con Obj y Arrays

--- objetos.js
+++ objetos.js
@@ -20,3 +20,42 @@ console.log(camion);
 camion[0].entrega = false;
 
 //mostramos resultados
+
+
+//Practica adicional -Ejercicio
+
+const personaArreglo = ['Carlos', 27, 'correo@mail.com', true, true];
+
+const persona = {
+    nombre: 'jesur',
+    edad: '25',
+    correo: 'correo@mail.com',
+    subscripciones: {
+        web: true,
+        correo: true,
+    },
+
+    coloresFavoritos: ['Negro', 'Rojo'],
+    saludo: function () {
+        alert('Esta es una alerta');
+    },
+};
+
+console.log('---------------------');
+console.log(persona.coloresFavoritos);
+console.log(persona['edad']);//entre comillas
+
+const variable = 'correo';
+console.log(persona[variable]);
+
+//Accediendo a la suscripcion
+console.log(persona.subscripciones.web);
+
+//agregando nuevo valor al objeto
+persona.pais = 'Mexico';
+persona.pais = 'España';
+persona.city = 'Tijuana';
+
+console.log(persona);
+
+
