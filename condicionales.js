d316c8ce5d7e72802de5bce43ed77fb78d5f74d2
jesur <juansurianodev@gmail.com>
2026-04-18 23:30:26 -0600

    Practicando condicionales

--- condicionales.js
+++ condicionales.js
@@ -30,13 +30,32 @@ if (herramientas.length > 3) {
 //Ejercicio 3.
 /*Quieres saber si la caja está llena Y 
 si además tienes un 'Machete' dentro. */
-const herramientas2 = ['Martillo', 'Pala', 'Pulidora', 'Taladro', 'Tornillo'];
+const herramientas2 = ['Martillo', 'Pala', 'Pulidora', 'Taladro', 'Tornillo', 'Machete'];
+//const herramientas2 = ['Martillo', 'Pala', 'Pulidora', 'Taladro', 'Tornillo'];
+
 const buscarHerr = 'Machete';
 const herrBuscada = herramientas2.indexOf(buscarHerr);
 
-if (herramientas2.length > 3 && herramientas2.includes('Machete')) {
+if (herramientas2.length > 3 && herramientas2[herrBuscada]) {
     console.log(`Caja llena ${herramientas2[herrBuscada]} en caja`);
 } else {
     console.log(`Aun cabe mas en la caja, ${buscarHerr} no tienes aun`);
 }
 
+//Ejercicio 4
+//Combinando operadores.
+persona2 = {
+    nombre: 'Jesur',
+    edad: 27,
+    pais: 'El Salvador',
+    ticket: false,
+};
+
+//puedo usar esta forma 'persona2.ticket === true'
+if (persona2.edad >= 18 && persona2.ticket) {
+    console.log('Eres mayor de edad y tienes ticket')
+} else if (persona2.edad >= 18 && !persona2.ticket) {
+    console.log('Eres mayor de edad pero No tienes ticket')
+} else {
+    console.log('No cumples ningun requisito para ingresar')
+}
